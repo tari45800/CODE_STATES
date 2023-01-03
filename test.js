086e63e3ee757ee238091f6712a6772262ba6778
tari45800 <tari45800@gmail.com>
2023-01-04 08:58:34 +0900

    0104 오전 커밋

--- test.js
+++ test.js
@@ -1,31 +1,14 @@
-/*
-21_mostFrequentCharacter
-문자열을 입력받아 가장 많이 반복되는 문자(letter)를 리턴해야 합니다.
-*/
-
-function mostFrequentCharacter(str) {
-
-  // 수도코드
-  // 시간이 없는 관계로 이하 생략
-  
-  let num = 0;
-  let s = ' ';
-  let arr = {};
-
-  for(let i of str){
-    if(!(i in arr) && i !== ' '){
-      arr[i] = 1;
-    } else {
-      arr[i]++;
-    }
-
-    if(arr[i] > num){
-      num = arr[i];
-      s = i;
-    }
-  }
-
-  return s;
+function whois({displayName: displayName, fullName: {firstName: name}}){
+  console.log(displayName + " is " + name);
 }
-let output = mostFrequentCharacter('apples not oranges');
-console.log(output); // --> 'p'
+
+let user = {
+  id: 42,
+  displayName: "jdoe",
+  fullName: {
+      firstName: "John",
+      lastName: "Doe"
+  }
+};
+
+whois(user)// 질문: 콘솔에서 어떻게 출력될까요?
