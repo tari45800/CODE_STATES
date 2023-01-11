1f61513f2d4262fc20363def73dd497a2cb7ca2d
tari45800 <tari45800@gmail.com>
2023-01-11 23:50:16 +0900

    0111 오후커밋

--- test.js
+++ test.js
@@ -1,14 +1,37 @@
-function whois({displayName: displayName, fullName: {firstName: name}}){
-  console.log(displayName + " is " + name);
+function solution(storey) {
+  //수도코드 
+  //storey를 문자열로 변환해 각 인덱스 값에 접근합니다.
+  //0부터 storey의 자릿수만큼 반복합니다.
+    //만약 storey의 반복횟수번 인덱스 값, num이 5이하라면 answer에 num을 더해줍니다.
+    //아니라면 answer에 11 - num을 더해줍니다.
+
+  var answer = 0;
+  let p = 0;
+  storey = storey.toString()
+
+  for(let i = storey.length-1; i >= 0; i--){
+    let num = Number(storey[i]) + p;
+
+    alert(num)
+    alert(answer)
+
+    if(num <= 5){
+      answer += num
+      p = 0; 
+    } else {
+      answer += 10 - num; 
+      p++
+    }
+
+
+  }
+  return answer;
 }
 
-let user = {
-  id: 42,
-  displayName: "jdoe",
-  fullName: {
-      firstName: "John",
-      lastName: "Doe"
-  }
-};
+//수도코드
+// 9999 - 1000 -1
+// 88 -100 -10 -1 -1
+// 16 - 10 -10 -1 -1
 
-whois(user)// 질문: 콘솔에서 어떻게 출력될까요?
+alert(solution(99))
+
