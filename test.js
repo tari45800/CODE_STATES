c2667e8cd9b7feb700358e54e6e1b89a9faef9c4
tari45800 <tari45800@gmail.com>
2023-01-16 22:44:55 +0900

    0116 오후커밋

--- test.js
+++ test.js
@@ -1,37 +1,20 @@
-function solution(storey) {
-  //수도코드 
-  //storey를 문자열로 변환해 각 인덱스 값에 접근합니다.
-  //0부터 storey의 자릿수만큼 반복합니다.
-    //만약 storey의 반복횟수번 인덱스 값, num이 5이하라면 answer에 num을 더해줍니다.
-    //아니라면 answer에 11 - num을 더해줍니다.
-
-  var answer = 0;
-  let p = 0;
-  storey = storey.toString()
-
-  for(let i = storey.length-1; i >= 0; i--){
-    let num = Number(storey[i]) + p;
-
-    alert(num)
-    alert(answer)
-
-    if(num <= 5){
-      answer += num
-      p = 0; 
-    } else {
-      answer += 10 - num; 
-      p++
-    }
-
-
+function computeWhenDouble(interestRate) {
+  // TODO: 여기에 코드를 작성합니다.
+  // 수도코드
+  // 반복한다. x가 두배가 될때 까지 2배가 될때 까지
+  
+  let i = 100;
+  let c = 0
+  
+  while(i < 200){
+    i = i + (i * interestRate / 100); 
+    c++
+    alert(i)
   }
-  return answer;
+
+
+  return c;
 }
 
-//수도코드
-// 9999 - 1000 -1
-// 88 -100 -10 -1 -1
-// 16 - 10 -10 -1 -1
-
-alert(solution(99))
-
+let output = computeWhenDouble(100);
+console.log(output); // --> 11
