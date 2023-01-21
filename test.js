dabafb015b72c33996c4e13db943359cd6c9009b
tari45800@gmail.com <tari45800@gmail.com>
2023-01-22 00:14:30 +0900

    0122 오전커밋

--- test.js
+++ test.js
@@ -1,20 +1,15 @@
-function computeWhenDouble(interestRate) {
-  // TODO: 여기에 코드를 작성합니다.
-  // 수도코드
-  // 반복한다. x가 두배가 될때 까지 2배가 될때 까지
-  
-  let i = 100;
-  let c = 0
-  
-  while(i < 200){
-    i = i + (i * interestRate / 100); 
-    c++
-    alert(i)
-  }
-
-
-  return c;
+const printString = (string) => {
+  setTimeout(function(){
+    console.log(string);
+  },
+  Math.floor(Math.random() * 100) + 1
+  )
 }
 
-let output = computeWhenDouble(100);
-console.log(output); // --> 11
+const printAll = () => {
+  printString("A");
+  printString("B");
+  printString("C");
+}
+
+printAll();
